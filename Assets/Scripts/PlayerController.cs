40bccc4f4517afc778f1a26039088533aed62d45
April <admdizonn@gmail.com>
2020-08-18 21:32:55 -0400

    Init and player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float moveSpeed;
+    private bool isMoving;
+    private Vector2 moveVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get axis input for movement.
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        isMoving = moveInput != Vector2.zero;
+        moveVelocity = moveInput.normalized * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        // Apply movement.
+        //rb.velocity = moveVelocity;
+        if (isMoving) rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
