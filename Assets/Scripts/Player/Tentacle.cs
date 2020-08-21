a2fe9780e15e2c4602426055a769da61cf7afc4c
April <admdizonn@gmail.com>
2020-08-20 21:32:16 -0400

    Implemented coto

---
+++ Assets/Scripts/Player/Tentacle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tentacle : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody2D rb;
+    public Vector2 verticalClamp;
+    public Vector2 horizontalClamp;
+    public float smoothThreshold;
+
+    public float moveSpeed;
+    private Vector2 moveVelocity;
+    private Vector2 mousePosition;
+    private Vector2 direction;
+
+
+    private void FixedUpdate()
+    {
+        // Find where the mouse is to determine object movement.
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = (mousePosition - (Vector2)rb.position).normalized;
+
+        // Smoothen movement and apply it to the rigidbody.
+        moveVelocity = direction * moveSpeed * Time.fixedDeltaTime;
+        rb.velocity = moveVelocity;
+    }
+
+    private void OnDisable()
+    {
+        rb.velocity = Vector2.zero;
+    }
+}
---
+++ Assets/Scripts/Player/TentacleController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleController : MonoBehaviour
+{
+
+    public Tentacle[] tentacles;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            tentacles[0].enabled = true;
+        }
+        else
+        {
+            tentacles[0].enabled = false;
+        }
+
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            tentacles[1].enabled = true;
+        }
+        else
+        {
+            tentacles[1].enabled = false;
+        }
+
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            tentacles[2].enabled = true;
+        }
+        else
+        {
+            tentacles[2].enabled = false;
+        }
+
+        if (Input.GetKey(KeyCode.Alpha4))
+        {
+            tentacles[3].enabled = true;
+        }
+        else
+        {
+            tentacles[3].enabled = false;
+        }
+    }
+
+}
